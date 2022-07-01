fb82dc4037f253bd962080ef8b02c524e9d34047
aleracing22 <108311876+aleracing22@users.noreply.github.com>
2022-07-01 00:36:21 -0300

    testing

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -201,6 +201,8 @@ function esVerdadero(valor){
 }
 esVerdadero(1)
 
+// test
+
 function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
